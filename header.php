23cc11cc0df823376c548f50d0400e2b22e3d92c
Erin <hoople.erin@gmail.com>
2014-02-08 16:36:21 -0600

    added clear fix to main page element

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
+<div id="page" class="hfeed site clearfix">
 	<?php do_action( 'before' ); ?>
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
